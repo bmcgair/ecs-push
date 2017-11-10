3357facae940e7c824af48d442a50d32527be1d8
Bill McGair <bill@mcgair.com>
2017-11-10 09:46:00 -0800

    add kube config file

--- bin/kube-push.sh
+++ bin/kube-push.sh
@@ -6,6 +6,8 @@ source ./bin/env-kube.sh
 docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 docker push "$DOCKER_REPO"/"$DOCKER_WEB_IMAGE":latest
 
+mkdir ${HOME}/.kube
+cp cluster-cfg ${HOME}/.kube/config
 kubectl config set clusters.uswest2.billm.foo.today.certificate-authority-data "$KUBE_CLUSTER_CERTIFICATE"
 kubectl config set users.uswest2.bill.foo.today.client-certificate-data "$KUBE_CLIENT_CERTIFICATE"
 kubectl config set users.uswest2.bill.foo.today.client-key-data "$KUBE_CLIENT_KEY"
