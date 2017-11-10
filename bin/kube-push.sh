5daaa39c29854325e58659104f447b3555c1b044
Bill McGair <bill@mcgair.com>
2017-11-09 20:50:15 -0800

    config on cmdline

--- bin/kube-push.sh
+++ bin/kube-push.sh
@@ -5,12 +5,10 @@ source ./bin/env-kube.sh
 
 docker push "$DOCKER_REPO"/"$DOCKER_WEB_IMAGE":latest
 
-mkdir ${HOME}/.kube
-cp cluster-cfg ${HOME}/.kube/config
 kubectl config set clusters.uswest2.billm.foo.today.certificate-authority-data "$KUBE_CLUSTER_CERTIFICATE"
 kubectl config set users.uswest2.bill.foo.today.client-certificate-data "$KUBE_CLIENT_CERTIFICATE"
 kubectl config set users.uswest2.bill.foo.today.client-key-data "$KUBE_CLIENT_KEY"
 
-kubectl create -f web-claim0-persistentvolumeclaim.json
-kubectl create -f web-deployment.json
-kubectl create -f web-service.json
+kubectl --kubeconfig cluster-cfg create -f web-claim0-persistentvolumeclaim.json
+kubectl --kubeconfig cluster-cfg create -f web-deployment.json
+kubectl --kubeconfig cluster-cfg create -f web-service.json
