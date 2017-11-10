1539ce4151d65f8d219c95b3be3e8d960af983d2
Bill McGair <bill@mcgair.com>
2017-11-09 20:36:23 -0800

    add kube config

--- bin/kube-push.sh
+++ bin/kube-push.sh
@@ -3,11 +3,14 @@
 # Comment
 source ./bin/env-kube.sh
 
-#if [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
-    docker push "$DOCKER_REPO"/"$DOCKER_WEB_IMAGE":latest
-    kops import cluster --name uswest2.bill.foo.today --region us-west-2 --state=$KOPS_STATE_STORE
-    kubectl create -f web-deployment.json
-    kubectl create -f web-service.json
-#    ecs-cli compose --project-name "$ECS_PROJECT" --file ecs-deploy.yml service up;
-#fi
+docker push "$DOCKER_REPO"/"$DOCKER_WEB_IMAGE":latest
+
+mkdir ${HOME}/.kube
+cp cluster-cfg ${HOME}/.kube/config
+kubectl config set clusters.uswest2.billm.foo.today.certificate-authority-data "$KUBE_CLUSTER_CERTIFICATE"
+kubectl config set users.uswest2.bill.foo.today.client-certificate-data "$KUBE_CLIENT_CERTIFICATE"
+kubectl config set users.uswest2.bill.foo.today.client-key-data "$KUBE_CLIENT_KEY"
+
+kubectl create -f web-claim0-persistentvolumeclaim.json
+kubectl create -f web-deployment.json
+kubectl create -f web-service.json
