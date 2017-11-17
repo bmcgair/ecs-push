757014f5941fe75c159619d5b8b5587f733c2996
Bill McGair <bill@mcgair.com>
2017-11-16 17:19:36 -0800

    try update

--- bin/kube-push.sh
+++ bin/kube-push.sh
@@ -3,15 +3,13 @@
 # Comment
 source ./bin/env-kube.sh
 
-docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
-docker push "$DOCKER_REPO"/"$DOCKER_WEB_IMAGE":latest
+docker login --username "$DOCKER_REPO" --password "$DOCKER_PASSWORD"
+docker push "$DOCKER_REPO"/"$DOCKER_WEB_IMAGE":"$TRAVIS_COMMIT"
 
 mkdir ${HOME}/.kube
 cp cluster-cfg ${HOME}/.kube/config
-kubectl config set clusters.uswest2.billm.foo.today.certificate-authority-data "$KUBE_CLUSTER_CERTIFICATE"
-kubectl config set users.uswest2.bill.foo.today.client-certificate-data "$KUBE_CLIENT_CERTIFICATE"
-kubectl config set users.uswest2.bill.foo.today.client-key-data "$KUBE_CLIENT_KEY"
+kubectl config set clusters.uswest2a.billm.foo.today.certificate-authority-data "$KUBE_CLUSTER_CERTIFICATE"
+kubectl config set users.uswest2a.bill.foo.today.client-certificate-data "$KUBE_CLIENT_CERTIFICATE"
+kubectl config set users.uswest2a.bill.foo.today.client-key-data "$KUBE_CLIENT_KEY"
 
-kubectl --insecure-skip-tls-verify=true create -f web-claim0-persistentvolumeclaim.json
-kubectl --insecure-skip-tls-verify=true create -f web-deployment.json
-kubectl --insecure-skip-tls-verify=true create -f web-service.json
+kubectl set image deployment/bdjweb ghost:"$TRAVIS_COMMIT"
