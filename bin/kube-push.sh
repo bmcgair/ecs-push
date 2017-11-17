2a93bc8d72df25eb11d89c7bc31f9459a8b715bf
Bill McGair <bill@mcgair.com>
2017-11-16 20:03:57 -0800

    skip certr

--- bin/kube-push.sh
+++ bin/kube-push.sh
@@ -14,4 +14,4 @@ kubectl config set clusters.uswest2a.billm.foo.today.certificate-authority-data 
 kubectl config set users.uswest2a.bill.foo.today.client-certificate-data "$KUBE_CLIENT_CERTIFICATE"
 kubectl config set users.uswest2a.bill.foo.today.client-key-data "$KUBE_CLIENT_KEY"
 
-kubectl set image deployment/fooweb2 bmcgair/django-web:$TRAVIS_COMMIT
+kubectl --insecure-skip-tls-verify=true set image deployment/fooweb2 bmcgair/django-web:$TRAVIS_COMMIT
